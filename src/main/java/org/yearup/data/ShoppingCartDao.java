b89f8657f8258aded465da93fa1f2373a5e7b1e8
Ivalene <ivalenelarrea234@gmail.com>
2024-12-17 15:54:51 -0600

    Shopping cart DAO

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -4,6 +4,16 @@ import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
-    ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    // Retrieve the shopping cart for a user
+    ShoppingCart getCartByUserId(int userId);
+
+    // Add a product to the user's shopping cart
+    void addProductToCart(int userId, int productId);
+
+    // Update the quantity of a product in the cart
+    void updateProductQuantity(int userId, int productId, int quantity);
+
+    // Clear all products from the user's shopping cart
+    void clearCart(int userId);
 }
+
